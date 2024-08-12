5250921fdc21bebef0713be26aaa75be00ca2c15
Rounak Mishra <133516852+rounakm535@users.noreply.github.com>
2024-08-13 00:21:11 +0530

    Time: 56 ms (100%), Space: 16 MB (69.43%) - LeetHub

---
+++ 0789-kth-largest-element-in-a-stream/0789-kth-largest-element-in-a-stream.py
@@ -0,0 +1,30 @@
+import heapq
+
+class KthLargest(object):
+
+    def __init__(self, k, nums):
+        """
+        :type k: int
+        :type nums: List[int]
+        """
+        self.k = k
+        self.heap = []
+        
+        # Initialize the heap with the first k elements
+        for num in nums:
+            if len(self.heap) < k:
+                heapq.heappush(self.heap, num)
+            else:
+                heapq.heappushpop(self.heap, num)
+
+    def add(self, val):
+        """
+        :type val: int
+        :rtype: int
+        """
+        if len(self.heap) < self.k:
+            heapq.heappush(self.heap, val)
+        elif val > self.heap[0]:
+            heapq.heapreplace(self.heap, val)
+        
+        return self.heap[0]
